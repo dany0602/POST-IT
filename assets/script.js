5090f5a8d4177e5cdd990efc57dca5c4567edcdf
danielleclerc <daniel.leclerc0602@gmail.com>
2024-12-12 17:27:13 +0100

    exercice

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,16 @@ document.getElementById("valider").addEventListener("click", function () {
     if (titre != "" && description != ""){
         console.log(titre);
         
-        document.getElementById("post-it").innerHTML += `<div>
-  <p>${titre}</p>
-  <p>${description}</p>
-</div>`
+        document.getElementById("post-it").innerHTML += `<div class="card bg-warning m-2 shadow col-lg-4 col-8 h-25">
+        <div class="card-body ">
+          <div class="row justify-content-between">
+            <h5 class="card-title col-8">${titre}</h5>
+            <button type="button" class="btn-close mx-2 col-3" aria-label="Close"></button>
+          </div>
+          <p class="card-text">${description}</p>
+        </div>
+      </div>`
     }})
+    document.getElementById("annuler").addEventListener("click", function(){
+document.getElementById("post-it").innerHTML = ""
+ })
